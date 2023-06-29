4da6952a4a9e46bba713d1351329a044016e0253
Jasper Van Dyck <jasper.vandyck@hotmail.be>
2023-06-29 23:41:23 +0200

    automatically refresh posts when creating new post

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -1,13 +1,8 @@
 import { useSession } from "next-auth/react";
 import { Button } from "./Button";
 import { ProfileImage } from "./ProfileImage";
-import {
-  FormEvent,
-  useCallback,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from "react";
+import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import type { FormEvent } from "react";
 import { api } from "~/utils/api";
 
 function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
@@ -32,14 +27,43 @@ function Form() {
     textAreaRef.current = textArea;
   }, []);
 
+  const trcpUtils = api.useContext();
+
   useLayoutEffect(() => {
     updateTextAreaSize(textAreaRef.current);
   }, [inputValue]);
 
   const createPost = api.post.create.useMutation({
     onSuccess: (newPost) => {
-      console.log(newPost);
       setInputValue("");
+
+      if (session.status !== "authenticated") return;
+
+      trcpUtils.post.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (oldData == null || oldData.pages[0] == null) return;
+
+        const newCachedPost = {
+          ...newPost,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data.user.id,
+            name: session.data.user.name || null,
+            image: session.data.user.image || null,
+          },
+        };
+
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              data: [newCachedPost, ...oldData.pages[0].data],
+            },
+            ...oldData.pages.slice(1),
+          ],
+        };
+      });
     },
   });
 
