aba10a9b8f133eb56ead8d06449937af6b0bd5f3
Jasper Van Dyck <jasper.vandyck@hotmail.be>
2023-06-29 22:00:11 +0200

    show infinte feed of posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { type NextPage } from "next";
+import { InfinitePostList } from "~/components/InfinitePostList";
 import { NewPostForm } from "~/components/NewPostForm";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   return (
@@ -8,8 +10,28 @@ const Home: NextPage = () => {
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
       </header>
       <NewPostForm />
+      <RecentPosts />
     </>
   );
 };
+
+function RecentPosts() {
+  const posts = api.post.infiniteFeed.useInfiniteQuery(
+    {},
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  return (
+    <InfinitePostList
+      posts={posts.data?.pages.flatMap((page) => page.data)}
+      isError={posts.isError}
+      isLoading={posts.isLoading}
+      hasMore={posts.hasNextPage}
+      fetchNewPosts={posts.fetchNextPage()}
+    />
+  );
+}
 
 export default Home;
