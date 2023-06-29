7e76e2a76a587d9a8e2e4939d14b0363f34f929c
Jasper Van Dyck <jasper.vandyck@hotmail.be>
2023-06-29 20:25:32 +0200

    SideNav bar met login en logout functionaliteit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+
 import { api } from "~/utils/api";
+
 import "~/styles/globals.css";
+import Head from "next/head";
+import { SideNav } from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,7 +14,19 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>AOT Hub</title>
+        <meta
+          name="description"
+          content="This is an Attack On Titan social platform "
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x"></div>
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
